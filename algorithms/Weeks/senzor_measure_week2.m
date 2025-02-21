5379d6d7f86b3219419d2446b5bf7946e8387564
msukd <msukd@DESKTOP-SQ19MQP>
2025-02-21 14:59:25 +0100

    Week 2 done

---
+++ algorithms/Weeks/senzor_measure_week2.m
@@ -0,0 +1,61 @@
+%% Week 2
+%Task 2
+function [public_vars] = senzor_measure_week2(read_only_vars, public_vars)
+soubor='senzor_data.mat';
+if read_only_vars.counter<100
+    if isfile(soubor)
+         data = load(soubor);
+         dataset=data.dataset;
+    else
+    dataset = []; % Pokud soubor neexistuje, vytvoříme nový dataset
+    end
+    dataset=[dataset;read_only_vars.gnss_position(1,1:end), read_only_vars.lidar_distances(1,1:end)];
+    save(soubor, 'dataset');
+else
+    dataset = load(soubor);
+    sigma=std(dataset.dataset)
+    mu=mean(dataset.dataset)
+        figure(2)
+        subplot(2,1,1)
+    histogram(dataset.dataset(1:end,1))
+    title('Souradnice X');
+    xlabel('Hodnota');
+    ylabel('Četnost');
+        subplot(2,1,2)
+    histogram(dataset.dataset(1:end,2))
+    title('Souradnice Y');
+    xlabel('Hodnota');
+    ylabel('Četnost');
+
+        figure(3)
+    for i=1:8
+    subplot(2,4,i)
+    histogram(dataset.dataset(1:end,i+2))
+    title(['Senzor ', num2str(i)]);
+    xlabel('Vzdalenost');
+    ylabel('Četnost');
+    end
+    %task 3
+    GNSS_cov=cov(dataset.dataset(1:end,1:2))
+    Lidar_cov=cov(dataset.dataset(1:end,3:10))
+    sqrt_std=sigma.^2
+    %task 4
+    x = -2.5:0.01:2.5;
+    gnss_pdf=norm_pdf(x,0,sigma(1));
+    Lidar_pdf=norm_pdf(x,0,sigma(3));
+    figure(4)
+    plot(x,gnss_pdf)
+    hold on
+    plot(x,Lidar_pdf)
+    hold off
+    title('Noise Characteristics of Robot Sensors');
+    xlabel('x');
+    ylabel('Probability Density');
+
+    error("konec mereni")
+end
+end
+
+function [pdf] = norm_pdf(x, mu,sigma)
+    pdf=(1/(sigma*sqrt(2*pi))) * exp(-0.5*((x-mu)/sigma).^2);
+end
--- algorithms/motion_control/plan_motion.m
+++ algorithms/motion_control/plan_motion.m
@@ -9,6 +9,19 @@ target = get_target(public_vars.estimated_pose, public_vars.path);
 % II. Compute motion vector
 
 public_vars.motion_vector = [0, 0];
+count=read_only_vars.counter;
+% switch count
+%     case num2cell(1:50)
+%     public_vars.motion_vector = [1, 1];
+%     case num2cell(51:110)
+%     public_vars.motion_vector = [0.9 , 1];
+%     case num2cell(111:139)
+%     public_vars.motion_vector = [1, 1];
+%     case num2cell(140:195)
+%     public_vars.motion_vector = [1 , 0.9];
+%     case num2cell(196:300)
+%     public_vars.motion_vector = [1, 1];
+% end
 
 
 end
